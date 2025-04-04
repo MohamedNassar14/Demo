f890fd483086e83ac4f98f2741c92d222bef50dd
MohamedNassar14 <mohamednassar5678@gmail.com>
2025-04-04 19:13:57 +0200

    Refactor navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        roboto: ['Roboto', 'sans-serif'],  // إضافة خط روبوتو
+        mainFont: ['Roboto', 'sans-serif']
       },
       colors: {
         'mainColor': '#680a08',
