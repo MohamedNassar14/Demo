9ce45a7feb5774db9ef5805741d101e2ce7d857f
MohamedNassar14 <mohamednassar5678@gmail.com>
2025-04-24 20:31:57 +0200

    Update arrows in carousel

--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,6 +1,6 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
 import { SliderModule } from 'primeng/slider';
-import { CarouselModule } from 'primeng/carousel';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 import { ButtonModule } from 'primeng/button';
 import { TagModule } from 'primeng/tag'
 import { CommonModule } from '@angular/common';
@@ -15,10 +15,46 @@ import { CommonModule } from '@angular/common';
 export class CarouselComponent {
 
 
-
   images: string[] = [
     'assets/images/villa-house-1.webp',
     'assets/images/villa-house-2.webp',
     'assets/images/villa-house-3.webp'
   ];
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    autoplay: true, 
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 1
+      },
+      940: {
+        items: 1
+      }
+    },
+    nav: false
+  }
+
+
+  @ViewChild('owlCar') owlCar: any;
+
+nextSlide() {
+  this.owlCar.next();
+}
+
+prevSlide() {
+  this.owlCar.prev();
+}
 }
