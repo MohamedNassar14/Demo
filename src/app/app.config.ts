ea1dea170d8eb634dab7d5f2f65ab219400134f6
MohamedNassar14 <mohamednassar5678@gmail.com>
2025-04-26 16:36:49 +0300

    Added Preload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, PreloadAllModules, withPreloading  } from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
- export const appConfig: ApplicationConfig = {  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),  provideHttpClient(withFetch()), provideAnimations(), provideAnimations(),   
+ export const appConfig: ApplicationConfig = {  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes , withPreloading(PreloadAllModules)),  provideHttpClient(withFetch()), provideAnimations(), provideAnimations(),   
       provideToastr({
             timeOut: 3000,
             positionClass: 'toast-top-left',
